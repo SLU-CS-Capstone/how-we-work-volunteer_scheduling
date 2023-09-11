70f399f1a0fc98cea9e39020b8ae35ea678c71f8
Abhilash Kotha <abhilash.kotha@slu.edu>
2023-09-11 17:03:34 -0500

    pdf code added

--- src/generator.py
+++ src/generator.py
@@ -2,5 +2,10 @@ from maze import Maze
 
 maze = Maze(20)
 maze.generate_maze()
-maze.print()
+choice = input("do you want in pdf(1) or terminal (2)")
+if(choice == 1):
+    maze.print(True)
+if(choice==2):
+    maze.print(False)
+
 print("Welcome to 2D maze")
