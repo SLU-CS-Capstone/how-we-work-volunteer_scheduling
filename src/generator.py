a7b19fb7057b025ced4928c59ff29ada5afe6243
loganwyas <loganw9999@gmail.com>
2023-09-11 17:00:08 -0500

    Fix issue 1

--- src/generator.py
+++ src/generator.py
@@ -1,6 +1,10 @@
 from maze import Maze
+from sys import argv
 
-maze = Maze(20)
+size = 20
+if (len(argv) > 1):
+    size = int(argv[1])
+maze = Maze(size)
 maze.generate_maze()
 maze.print()
 print("Welcome to 2D maze")
